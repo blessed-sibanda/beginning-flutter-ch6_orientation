bc9b23d227e5789325e6f690ca03628fb4722194
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-11-30 19:42:45 +0200

    more orientation examples

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -16,12 +16,60 @@ class MyHomePage extends StatelessWidget {
             child: Column(
               children: const [
                 OrientationLayoutIconsWidget(),
+                Divider(),
+                OrientationLayoutWidget(),
+                Divider(),
+                GridViewWidget(),
+                Divider(),
               ],
             ),
           ),
         ),
       ),
     );
+  }
+}
+
+class GridViewWidget extends StatelessWidget {
+  const GridViewWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Orientation _orientation = MediaQuery.of(context).orientation;
+    return GridView.count(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      crossAxisCount: _orientation == Orientation.portrait ? 2 : 4,
+      childAspectRatio: 5.0,
+      children: List.generate(
+        8,
+        (index) => Text('Grid $index', textAlign: TextAlign.center),
+      ),
+    );
+  }
+}
+
+class OrientationLayoutWidget extends StatelessWidget {
+  const OrientationLayoutWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Orientation _orientation = MediaQuery.of(context).orientation;
+    return _orientation == Orientation.portrait
+        ? Container(
+            alignment: Alignment.center,
+            color: Colors.yellow,
+            height: 100.0,
+            width: 100.0,
+            child: const Text('Potrait'),
+          )
+        : Container(
+            alignment: Alignment.center,
+            color: Colors.lightGreen,
+            height: 100.0,
+            width: 200.0,
+            child: const Text('Landscape'),
+          );
   }
 }
 
